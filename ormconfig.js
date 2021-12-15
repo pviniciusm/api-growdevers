a62778af8c9641dda270e48b5cde43047131419d
pviniciusm <pvinics@gmail.com>
2021-12-14 23:49:28 -0300

    add: typeorm configuration

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+    type: "postgres",
+    host: "ec2-52-20-143-167.compute-1.amazonaws.com",
+    port: 5432,
+    username: "null",
+    password: "null",
+    database: "d2of6ib79e96id",
+    entities: [],
+    synchronize: false,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+};
