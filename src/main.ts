bc5638f9eddd4f8d8b769bf98e9c4fdc1c74c86d
pviniciusm <pvinics@gmail.com>
2021-12-14 23:54:27 -0300

    add: create database connection before express init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import { initConnection } from "./database/connection";
 import routes from "./routes";
+import growdeverRoutes from "./router/growdever-routes";
 
 const app = express();
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
-app.listen(8081, () => console.log("Server iniciou"));
+app.use("/db-growdevers", growdeverRoutes);
+
+initConnection()
+    .then(() => app.listen(8081, () => console.log("Server iniciou")))
+    .catch((error) => {
+        console.log("Error at creating connection with database");
+        console.log(error);
+    });
